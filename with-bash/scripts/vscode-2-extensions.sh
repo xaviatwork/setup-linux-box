7f82e6ee1ab595adeeecf9c8e89860fb0e9f402a
Xavi Aznar <xavi.at.work@icloud.com>
2023-08-06 08:44:25 +0200

    feat: instala extensiones en VSCode
    - MDLint
    - Tema: Night OWl -> actualiza los settings de usuario
    - Iconos: vscode-icons -> actualza los settings de usuario
    - Remove Development extension pack
    - Extensión de Go
    
    Al no usar proxy, los problemas de VSCode con el certificado insertado
    por el proxy han desaparecido y es posible instalar extensiones
    usando el método "nativo" y directamente desde el Marketplace.
    
    Como no uso la sincronización de la configuración en VSCode, configuro
    las preferencias de usuario a través del fichero (usando Jq).

---
+++ with-bash/scripts/vscode-2-extensions.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+vscode_user_settings_file="$HOME/.config/Code/User/settings.json"
+
+if [[ ! -f ${vscode_user_settings_file} ]]; then
+    echo "Create empty user's settings.json file"
+    echo "{}" >> ${vscode_user_settings_file}
+fi 
+echo "Extension: MardownLint"
+code --install-extension DavidAnson.vscode-markdownlint
+echo "Theme: Night Owl"
+code --install-extension sdras.night-owl
+current_theme=$(jq '."workbench.colorTheme"' ${vscode_user_settings_file}) 
+if [[ ${current_theme} == "null" || ${current_theme} != "Night Owl" ]];then
+	jq '."workbench.colorTheme"="Night Owl"' ${vscode_user_settings_file} > ${vscode_user_settings_file}.tmp && mv  ${vscode_user_settings_file}.tmp ${vscode_user_settings_file}
+fi
+
+echo "Icons: VSCode Icons"
+code --install-extension vscode-icons-team.vscode-icons
+
+current_icons=$(jq '."workbench.colorIcon' ${vscode_user_settings_file}) 
+if [[ ${current_icons} == "null" || ${current_theme} != "vscode-icons" ]];then
+	# jq '."workbench.colorTheme"=""' ${vscode_user_settings_file} > ${vscode_user_settings_file}.tmp && mv  ${vscode_user_settings_file}.tmp ${vscode_user_settings_file}
+fi
+if [[ $(jq '."workbench.iconTheme"' ${vscode_user_settings_file}) == "null" ]];then
+	jq '."workbench.iconTheme"="vscode-icons"' ${vscode_user_settings_file}
+fi
+echo "Extension: Remote Development"
+echo " - Remote SSH"
+echo " - Remote Tunnels"
+echo " - Dev Containers (Requires Docker)"
+echo " - WSL"
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+echo "Extension: Go"
+code --install-extension golang.Go
