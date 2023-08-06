73f58603bc7b3cd55fd56c4c4e27070b26f80434
Xavi Aznar <xavi.at.work@icloud.com>
2023-08-06 09:35:18 +0200

    feat: configura Iosevka Term como fuenta por defecto en VSCode

--- with-bash/scripts/iosevka-font.sh
+++ with-bash/scripts/iosevka-font.sh
@@ -15,11 +15,32 @@ function install_font(){
     fi
 }
 
+update_vscode_settings() {
+    vscode_user_settings_file="$HOME/.config/Code/User/settings.json"
+    if [[ ! -f ${vscode_user_settings_file} ]]; then
+        echo "[ INFO ] Create empty user's settings.json file"
+        echo "{}" >> ${vscode_user_settings_file}
+    fi 
+
+    echo "[ INFO ] Checking current font in VSCode"
+    current_fonts=$(jq -r '."editor.fontFamily"' ${vscode_user_settings_file})
+
+    if [[ ${current_fonts} == "null" ]]; then
+        echo "[ INFO ] Using default fonts (no user customization)"
+        current_fonts="'Droid Sans Mono', 'monospace', monospace"
+    fi
+    if ! echo ${current_fonts} | grep -i 'iosevka termm' ; then
+        fonts="'Iosevka Term', ${current_fonts}"
+        jq --arg fonts "${fonts}" '."editor.fontFamily" = $fonts' ${vscode_user_settings_file} > ${vscode_user_settings_file}.tmp && mv ${vscode_user_settings_file}.tmp ${vscode_user_settings_file}
+    fi
+}
+
+
 if [ -f ${fontIosevkaTermPkgName} ]
 then
     echo "[ INFO ] ${fontIosevkaTermPkgName} version ${fontIosevkaTerm_version} already downloaded..."
 else
     curl -JLO $fontIosevkaTermURL
 fi
-
-install_font
+# install_font
+update_vscode_settings
